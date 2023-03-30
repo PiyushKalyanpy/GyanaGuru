1d3d03a5dc6df891436b3901077ab7f3c849f5bc
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-03-31 00:51:20 +0530

    signup page  and state variable

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,25 @@
 import { ButtonWithImage } from "@/Components/components";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Login = () => {
-
-    
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const showPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+  const createAccount = () => {
+    router.push("/signup");
+  };
 
   return (
     <div className="flex w-full h-screen bg-gray-200 ">
-      <div className="flex flex-col w-10/12 lg:w-1/4 bg-white h-fit m-auto min-h-1/4 p-4 ">
+      <div className="flex flex-col w-10/12 md:w-8/12 lg:w-1/4 bg-white rounded-lg h-fit m-auto min-h-1/4 p-4 ">
         <div className="flex flex-col space-y-8 items-center ">
-
           {/* logo with title */}
           <div className="flex flex-row items-center space-x-4 ">
             <img src="/logo.svg" alt="logo" width={40} height={40} />
@@ -17,11 +28,9 @@ const Login = () => {
           </div>
 
           {/* login heading and text */}
-          <div className="flex items-center flex-col space-y-2 ">
+          <div className="flex w-full px-2 flex-col space-y-2 ">
             <h3 className="text-3xl font-medium ">Login</h3>
-            <h4 className="flex text-center px-4">
-              Welcome to the login page of Gyana Guru.
-            </h4>
+            <h4 className="flex">Welcome to the login page of Gyana Guru.</h4>
           </div>
 
           {/* login form */}
@@ -43,13 +52,30 @@ const Login = () => {
               <input
                 type="email"
                 placeholder="Email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
                 className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-2 focus:border-violet-500"
               />
-              <input
-                type="password"
-                placeholder="Password"
-                className="border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2"
-              />
+              <div className="flex w-full space-x-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  className="w-full border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2"
+                />
+                <div
+                  className="flex items-center text-zinc-500 border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2  "
+                  onClick={() => showPasswordToggle()}
+                >
+                  <span className="material-icons-outlined ">
+                    {showPassword ? "visibility" : "visibility_off"}
+                  </span>
+                </div>
+              </div>
+
               <p className="w-fit text-right start-left text-sm text-violet-800 hover:underline cursor-pointer">
                 Forgot Password?
               </p>
@@ -62,10 +88,13 @@ const Login = () => {
               </button>
             </div>
 
-            {/* forgot password */}
-            <div className="flex flex-col items-center mt-4 justify-between">
-              <p className="w-fit text-slate-600">Don't have an account</p>
-              <p className="w-fit text-violet-800 hover:underline cursor-pointer">
+            {/* Create Account */}
+            <div className="flex flex-col items-center mt-4 text-sm justify-between">
+              <p className="w-fit text-slate-600">Don&apos;t have an account</p>
+              <p
+                onClick={() => createAccount()}
+                className="w-fit text-violet-800 hover:underline cursor-pointer"
+              >
                 Create an account
               </p>
             </div>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,12 +1,120 @@
-import React, { useState } from "react";
-import { account } from "../database/appwrite";
+import { ButtonWithImage } from "@/Components/components";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const SignUp = () => {
- 
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const showPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+  const moveToSignIn = () => {
+    router.push("/login");
+  };
 
   return (
-    <div>
-      
+    <div className="flex w-full h-screen bg-gray-200 ">
+      <div className="flex flex-col w-10/12 md:w-8/12 lg:w-1/4 bg-white rounded-lg h-fit m-auto min-h-1/4 p-4 ">
+        <div className="flex flex-col space-y-8 items-center ">
+          {/* logo with title */}
+          {/* <div className="flex flex-row items-center space-x-4 ">
+            <img src="/logo.svg" alt="logo" width={40} height={40} />
+
+            <h1 className="text-md font-semibold ">GyanaGuru</h1>
+          </div> */}
+
+          {/* login heading and text */}
+          <div className="flex w-full px-2 flex-col mt-8 space-y-2 ">
+            <h3 className="text-3xl font-semibold ">Sign Up</h3>
+            <h4 className="flex">Welcome to Gyana Guru. </h4>
+          </div>
+
+          {/* login form */}
+          <div className="flex flex-col space-y-4 w-full mx-4 ">
+            <ButtonWithImage
+              buttonName="Continue with Google"
+              icon="/images/google.svg"
+            />
+
+            {/* make or with divider */}
+            <div className="flex flex-row space-x-4 my-4 items-center">
+              <hr className="w-full border-gray-300" />
+              <h4 className="font-medium text-zinc-500">or</h4>
+              <hr className="w-full border-gray-300" />
+            </div>
+
+            {/* login with email and password */}
+            <div className="flex flex-col space-y-4">
+              {/* email */}
+              <input
+                type="email"
+                placeholder="Email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-2 focus:border-violet-500"
+              />
+              {/* password */}
+              <div className="flex w-full space-x-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  className="w-full border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2"
+                />
+                
+              </div>
+              {/* confirm password */}
+              <div className="flex w-full space-x-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={user.confirmPassword}
+                  onChange={(e) =>
+                    setUser({ ...user, confirmPassword: e.target.value })
+                  }
+                  className="w-full border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2"
+                />
+                <div
+                  className="flex items-center text-zinc-500 border focus:outline-none focus:border-2 focus:border-violet-500 border-gray-300 rounded-lg p-2  "
+                  onClick={() => showPasswordToggle()}
+                >
+                  <span className="material-icons-outlined ">
+                    {showPassword ? "visibility" : "visibility_off"}
+                  </span>
+                </div>
+              </div>
+
+              
+            </div>
+
+            {/* login button */}
+            <div className="flex flex-row space-x-4 py-4 transition hover:scale-[1.02]">
+              <button className="bg-violet-700 text-white rounded-lg p-2 w-full">
+                Login
+              </button>
+            </div>
+
+            {/* Create Account */}
+            <div className="flex flex-col items-center mt-4 text-sm justify-between">
+              <p className="w-fit text-slate-600">Already have an account</p>
+              <p
+                onClick={() => moveToSignIn()}
+                className="w-fit text-violet-800 hover:underline cursor-pointer"
+              >
+                Sign In to you account
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
