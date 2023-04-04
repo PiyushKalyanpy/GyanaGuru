3aae09729dd45dea6943815175c665c7b250ae83
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-04-04 17:02:20 +0530

    Bugs Fixed

--- Components/LandingFooter.tsx
+++ Components/LandingFooter.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
+
 const LandingFooter = () => {
+
     return (
         <div className="flex flex-row w-full border-y-2 items-center gap-8 border-black my-10 px-20  ">
             <div className="flex flex-row items-center">
-          <img src="./logo.svg" alt="logo" />
+          <Image width={40} height={40} src="./logo.svg" alt="logo" />
           <h1 className="font-archivo font-semibold text-lg ">GyanaGuru</h1>
         </div>
         <p>© 2023 Gyana Guru. All rights reserved.</p>
