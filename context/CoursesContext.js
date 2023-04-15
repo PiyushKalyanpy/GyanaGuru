15b74f646bca1dd2538533d367f9643ebd806065
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-04-15 21:15:59 +0530

    🪲 Bugs fixed + database updated

--- context/CoursesContext.js
+++ context/CoursesContext.js
@@ -16,6 +16,8 @@ import {
 } from "firebase/auth";
 import { app } from "../database/firebase";
 import { auth, db, provider } from "../database/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const CoursesContext = createContext({});
 
@@ -27,6 +29,8 @@ const CoursesProvider = ({ children }) => {
   const coursesCollection = useMemo(() => collection(db, "courses"), [db]);
   const usersCollection = useMemo(() => collection(db, "users"), [db]);
   const [currentUser, setCurrentUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   const categoriesCollection = useMemo(
     () => collection(db, "categories"),
@@ -34,43 +38,6 @@ const CoursesProvider = ({ children }) => {
   );
 
   const [courses, setCourses] = useState([]);
-  useEffect(() => {
-    const getCourses = async () => {
-      const coursesSnapshot = await getDocs(coursesCollection);
-      const coursesList = coursesSnapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-      });
-      setCourses(coursesList);
-      setLoading(false);
-    };
-    getCourses();
-  }, [coursesCollection]);
-
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const getUsers = async () => {
-      const usersSnapshot = await getDocs(usersCollection);
-      const usersList = usersSnapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-      });
-      setUsers(usersList);
-      setLoading(false);
-    };
-    getUsers();
-  }, [usersCollection]);
-
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    const getCategories = async () => {
-      const categoriesSnapshot = await getDocs(categoriesCollection);
-      const categoriesList = categoriesSnapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-      });
-      setCategories(categoriesList);
-      setLoading(false);
-    };
-    getCategories();
-  }, [categoriesCollection]);
 
   const addUserToFirebase = async (user) => {
     await setDoc(doc(db, "users", user && user.email), {
@@ -105,3 +72,41 @@ const CoursesProvider = ({ children }) => {
 };
 
 export { CoursesContext, CoursesProvider };
+
+// 🔖 this code should be removed during - code cleaning
+
+// useEffect(() => {
+//   const getCourses = async () => {
+//     const coursesSnapshot = await getDocs(coursesCollection);
+//     const coursesList = coursesSnapshot.docs.map((doc) => {
+//       return { id: doc.id, ...doc.data() };
+//     });
+//     setCourses(coursesList);
+//     setLoading(false);
+//   };
+//   getCourses();
+// }, [coursesCollection]);
+
+// useEffect(() => {
+//   const getUsers = async () => {
+//     const usersSnapshot = await getDocs(usersCollection);
+//     const usersList = usersSnapshot.docs.map((doc) => {
+//       return { id: doc.id, ...doc.data() };
+//     });
+//     setUsers(usersList);
+//     setLoading(false);
+//   };
+//   getUsers();
+// }, [usersCollection]);
+
+// useEffect(() => {
+//   const getCategories = async () => {
+//     const categoriesSnapshot = await getDocs(categoriesCollection);
+//     const categoriesList = categoriesSnapshot.docs.map((doc) => {
+//       return { id: doc.id, ...doc.data() };
+//     });
+//     setCategories(categoriesList);
+//     setLoading(false);
+//   };
+//   getCategories();
+// }, [categoriesCollection]);
