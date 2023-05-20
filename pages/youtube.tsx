e5f31909ee26829620711d8872c48291b8300fe0
Sathvik K <92165565+Sathvik902@users.noreply.github.com>
2023-05-21 00:41:18 +0530

    Function Based Component.

--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,35 +1,33 @@
 import React from "react";
 import YouTube from "react-youtube";
 
-export default class Youtube extends React.Component {
-  render() {
-    const opts = {
-      height: "390",
-      width: "640",
-      playerVars: {
-        autoplay: 1,
-        rel: 0,
-        showinfo: 0,
-        modestbranding: 1,
-        start: 300,
-      },
-    };
+const Youtube = () => {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+      rel: 0,
+      showinfo: 0,
+      modestbranding: 1,
+      start: 300,
+    },
+  };
 
-    return (
-      <div>
-        <h3>GeeksforGeeks - Youtube</h3>
+  return (
+    <div>
+      <h3>GeeksforGeeks - Youtube</h3>
 
-        <iframe
-          className="video"
-          title="Youtube player"
-        //   sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
-          src={`https://youtube.com/embed/Nr-gYadj5dw?autoplay=0`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
+      <iframe
+        className="video"
+        title="Youtube player"
+        src={`https://youtube.com/embed/Nr-gYadj5dw?autoplay=0`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
-        ></iframe>
-      </div>
-    );
-  }
-}
+export default Youtube;
