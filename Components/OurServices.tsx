e79770c94bf70f612320857007d6349cd190bdb1
Pratik <pratiksolanki2026@gmail.com>
2023-05-20 20:46:15 +0530

    This commit adds the categories in type of courses on clicking show more and also improves the styles of category buttons

--- Components/OurServices.tsx
+++ Components/OurServices.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import CourseData from "../data/course_categories.json";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+
 const OurServices = () => {
+
+  const [showallcourses, setCoursesToShow] = React.useState(false)
+  const courselimit = showallcourses ? CourseData.length : 6
+  const coursestoshow = CourseData.slice(0, courselimit)
+
   useEffect(() => {
     const elements = document.querySelectorAll(".transform");
 
@@ -52,12 +58,13 @@ const OurServices = () => {
             </div>
             <h2 className="text-xl font-archivo text-zinc-700 ">{`Get all types of courses`}</h2>
             <div className="flex flex-wrap w-full gap-4 ">
-              {CourseData.slice(0, 6).map((course, index) => {
+
+              {coursestoshow.map((course, index) => {
                 return (
                   <CourseCard key={index} courseName={course.categoryName} />
                 );
               })}
-              <CourseCard courseName="many more..." />
+              <Show onclick={() => {setCoursesToShow(!showallcourses)}} courseName={showallcourses? "Show less <" : "Show more >"} />
             </div>
           </div>
         </div>
@@ -68,10 +75,19 @@ const OurServices = () => {
 
 const CourseCard = ({ courseName }: any) => {
   return (
-    <div className="flex border-2 hover:border-black hover:scale-[1.04]  transition border-zinc-200 rounded-full px-4 py-2">
+    <button className="flex border-2 hover:border-black hover:scale-[1.04] hover:bg-blue-200 hover:border-blue-400 transition border-zinc-200 rounded-full px-4 py-2">
+      <h4 className="font-inter">{courseName}</h4>
+    </button>
+  );
+};
+
+const Show = ({ courseName, onclick }: any) => {
+  return (
+    <div onClick={onclick} className="font-medium bg-blue-200 border-2 border-blue-400 border-zinc-200 rounded-full cursor-pointer px-4 py-2">
       <h4 className="font-inter">{courseName}</h4>
     </div>
   );
 };
+
 
 export default OurServices;
