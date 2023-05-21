1f161cf1c869357cb6cfb4284012701a329e2598
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-05-22 02:51:14 +0530

    added contribute button

--- Components/HomeSidebar.tsx
+++ Components/HomeSidebar.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { LogoWithName } from "./components";
 import { setCookie } from "cookies-next";
-import {signOut } from 'firebase/auth'
+import Image from "next/image";
 
 const HomeSidebar = ({ pageNumber = 1 }: any) => {
   const router = useRouter();
@@ -16,6 +15,11 @@ const HomeSidebar = ({ pageNumber = 1 }: any) => {
     <div className="fixed top-0 flex  flex-col  p-4 w-fit h-screen justify-between ">
       {/* <LogoWithName width={40} height={40} /> */}
       <div className="flex flex-col space-y-4">
+        {/* logo */}
+        <div onClick={() => router.push('/')} className="flex cursor-pointer flex-row items-center space-x-4 ">
+          <Image src="/logo.svg" alt="logo" width={50} height={40} />
+        </div>
+
         <SidebarItem
           icon="home"
           Icon={DashboardIcon}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,6 +6,20 @@ const Dashboard = () => {
       <div className="col-span-1">
         <HomeSidebar pageNumber={1} />
       </div>
+      <div className="flex col-span-11 w-full h-full ">
+        {/* button to contribute to new dashboard design */}
+        <button
+          className="flex m-auto shadow-xl shadow-violet-200  text-white bg-violet-500  rounded-full p-4 hover:bg-violet-600"
+          onClick={() => {
+            window.open(
+              "https://github.com/PiyushKalyanpy/GyanaGuru/issues/48",
+              "_blank"
+            );
+          }}
+        >
+          Contribute to new dashboard design
+        </button>
+      </div>
     </div>
   );
 };
@@ -13,8 +27,10 @@ const Dashboard = () => {
 export default Dashboard;
 
 // basic template
-{/* <div className="grid grid-cols-12 w-full h-screen bg-white">
+{
+  /* <div className="grid grid-cols-12 w-full h-screen bg-white">
   <div className="col-span-2  border-r border-zinc-200">
     <HomeSidebar />
   </div>
-</div>; */}
+</div>; */
+}
