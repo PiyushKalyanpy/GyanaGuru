9d5e35222ef2e6b31a26cf40f6e2ad89ff313316
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-05-22 17:40:06 +0530

    Fixed : issue# #17

--- Components/LandingNav.tsx
+++ Components/LandingNav.tsx
@@ -8,30 +8,54 @@ const LandingNav = () => {
   const handleGetStartedClick = () => {
     router.push("/login");
   };
+  const handleCoursesClick = () => {
+    const courseSection = document.getElementById('courses');
+    if(courseSection){
+      courseSection.scrollIntoView({behavior:'smooth'});
+    }
+  };
+  const handleOurServicesClick = () => {
+    const ourserviesSection = document.getElementById('ourservices');
+    if(ourserviesSection){
+      ourserviesSection.scrollIntoView({behavior:"smooth"});
+    }
+  };
+  const handleBenefitsClick = () => {
+    const benefitsSection = document.getElementById('whychooseus');
+    if(benefitsSection){
+      benefitsSection.scrollIntoView({behavior:'smooth'});
+    }
+  };
+  const handleContactClick = () => {
+    const contactSection = document.getElementById('contact');
+    if(contactSection){
+      contactSection.scrollIntoView({behavior:"smooth"});
+    }
+  };
   return (
     <div className="sticky top-0 w-full border-b-2 border-white bg-white/30 backdrop-blur-md z-20">
       <div className="flex flex-row w-full justify-between items-center px-20 py-4 ">
         {/* Logo with Title */}
         <div className="flex flex-row items-center">
-          <img src="./logo.svg" alt="logo" />
-          <h1 className="font-archivo font-semibold text-lg ">GyanaGuru</h1>
+          <a href="#" ><img src="./logo.svg" alt="logo" /></a>
+          <a href="#"><h1 className="font-archivo font-semibold text-lg ">GyanaGuru</h1></a>
         </div>
         {/* Nav Links */}
         <div className="flex flex-row items-center gap-8">
-          <Link className={navLinkStyle} href="#">
+          <Link className={navLinkStyle} href ="#">
             Home
           </Link>
-          <Link className={navLinkStyle} href="#">
+          <Link className={navLinkStyle} href="#courses" onClick={handleCoursesClick}>
             Courses
           </Link>
-          <Link className={navLinkStyle} href="#">
+          <Link className={navLinkStyle} href="#ourservices" onClick={handleOurServicesClick}>
             Our Services
           </Link>
-          <Link className={navLinkStyle} href="#">
+          <Link className={navLinkStyle} href="#benefits" onClick={handleBenefitsClick}>
             Benefits
           </Link>
-          <Link className={navLinkStyle} href="#">
-            FAQ’s
+          <Link className={navLinkStyle} href="#contact" onClick={handleContactClick}>
+            Contact
           </Link>
         </div>
         {/* Get Started Button */}
