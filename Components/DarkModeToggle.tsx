cd224f1956c6f99e4d57349b46feda6bbf2b208a
Nikhil1017 <115337705+Nikhil1017@users.noreply.github.com>
2023-05-22 16:32:47 +0530

    .

--- Components/DarkModeToggle.tsx
+++ Components/DarkModeToggle.tsx
@@ -9,14 +9,14 @@ function DarkModeToggle() {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className=""
     >
       <Image
-      width={39}
+      width={36}
       height={40}
       src="/darksun.svg"
       alt="dark mode"
       className="hidden dark:block  hover:scale-110 duration-300"
       />
       <Image
-      width={40}
+      width={36}
       height={40}
       src="/darkmoon.svg"
       alt="light mode"
