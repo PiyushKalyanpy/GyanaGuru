223c74ef00fcaf612d9bac4d20f11cd8883e9df1
shruti8302 <93856491+shruti8302@users.noreply.github.com>
2023-05-24 23:55:23 +0530

    made changes in the footer section

--- Components/LandingFooter.tsx
+++ Components/LandingFooter.tsx
@@ -5,14 +5,14 @@ const LandingFooter = () => {
     const hoverEffect = "flex items-center transition ease-out duration-200 hover:text-black";
     return (
 
- <footer className="copyright-section">
+ <footer className="copyright-section dark:bg-zinc-700">
      
 <div className="w-80vw mx-auto max-w-1200px">
 {/* <Image width={40} height={40} src="./logo.svg" alt="logo" /> */}
 <div className="flex justify-around flex-wrap">
     
-    <div className="mt-10">
-        <div className="font-bold text-lg mb-3 text-black text-opacity-90">Quick Links</div>
+      <div className="mt-10">
+         <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Quick Links</div>
         <ul className="list-none m-0 p-0">
                <li className="leading-8 text-base text-black text-opacity-80">
                 <Link href="#" className={hoverEffect}>Home</Link>
@@ -33,7 +33,7 @@ const LandingFooter = () => {
         </div>
         
         <div className="mt-10">
-           <div className="font-bold text-lg mb-3 text-black text-opacity-90">Community</div>
+            <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Community</div>
            <ul>
                 <li className="leading-8 text-base text-black text-opacity-80">
                 <a href="https://github.com/PiyushKalyanpy/GyanaGuru" target="_blank" className={hoverEffect}><AiFillGithub className="mr-1" />Github</a>
@@ -54,7 +54,7 @@ const LandingFooter = () => {
         </div>
         
         <div className="mt-10">
-           <div className="font-bold text-lg mb-3 text-black text-opacity-90">Help</div>
+            <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Help</div>
            <ul>
                 <li className="leading-8 text-base text-black text-opacity-80">
                    <a href="https://github.com/PiyushKalyanpy/GyanaGuru/issues" target="_blank" className={hoverEffect}>Github Issues</a>
@@ -70,12 +70,12 @@ const LandingFooter = () => {
         </div>
 </div>
 <div className="flex flex-col justify-center w-full items-center gap-6">
-            <div className="flex flex-row items-center mt-6">
-          <Image width={60} height={60} src="./logo.svg" alt="logo" />
-          <h1 className="font-archivo font-bold text-2xl "><a href="#">GyanaGuru</a></h1>
-        </div>
-        <p className="mb-8 items-center">© 2023 Gyana Guru. All rights reserved.</p>
-        </div>
+  <div className="flex flex-row items-center mt-6">
+    <Image width={60} height={60} src="./logo.svg" alt="logo" />
+    <h1 className="font-archivo font-bold text-2xl "><a href="#">GyanaGuru</a></h1>
+  </div>
+  <p className="mb-8 items-center">© 2023 Gyana Guru. All rights reserved.</p>
+</div>
  </footer>
  
     );
