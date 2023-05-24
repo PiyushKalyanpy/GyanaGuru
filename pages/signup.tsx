97ee015af7e4a63e9f2dc72f0cbf3ce63d56ef81
Hemant2335 <knrt73373@gmail.com>
2023-05-24 23:36:06 +0530

    newcommit

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -28,7 +28,7 @@ const SignUp = () => {
   };
 
   const warning=()=>{
-    toast.warn('Please Complete all the fields', {
+    toast.warn('Please Complete all fields', {
       position: "bottom-right",
       autoClose: 5000,
       hideProgressBar: false,
