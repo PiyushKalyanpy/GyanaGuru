a0f1f80461bca5b872f77c4afd36a9a703e417b3
itsPriyanshuhere <priyanshupandey256@gmail.com>
2023-05-31 13:41:26 +0530

    Whenever the logo would be clicked the user would be routed to the homepage and changed cursor to pointer on hover above logoImage

--- Components/LogoWithName.tsx
+++ Components/LogoWithName.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 const LogoWithName = ({ width, height }: any) => {
+
+  const router = useRouter();
+
+  const handleClickonLogo = () => {
+    router.push("/");
+  };
+
   return (
-    <div className="flex  items-center   w-full h-fit  justify-center ">
+    <div className="flex  items-center   w-full h-fit  justify-center cursor-pointer " onClick={handleClickonLogo}>
       <Image src="./logo.svg" width={width} height={height} alt="logo" />
       <h1 className="font-archivo font-semibold text-xl ">GyanaGuru</h1>
     </div>
