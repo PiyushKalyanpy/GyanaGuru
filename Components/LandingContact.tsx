5f5d0fd250912d16b670ecd544e21dde4d5a87cb
shivaniyuvrajyadav <shivaniyuvarajyadav@gmail.com>
2023-06-01 22:22:54 +0530

    firebase file changed. also did some changes in LandingContact page.

--- Components/LandingContact.tsx
+++ Components/LandingContact.tsx
@@ -1,12 +1,35 @@
 import Image from "next/image";
+import emailjs from 'emailjs-com';
+import { useRef, useState } from "react";
 
 const LandingContact = () => {
+  const form = useRef();
+  const [isMessageSent, setIsMessageSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_rjqxpou', 'template_f8vouol', form.current, 'o2kgKJm8TdvQP4pBr')
+      .then(
+        (result) => {
+          console.log('Email sent successfully!', result.text);
+          setIsMessageSent(true);
+        },
+        (error) => {
+          console.error('Email sending failed:', error.text);
+          // You can handle the error here, such as displaying an error message to the user.
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="w-full h-fit p-20 dark:bg-neutral-950">
-      <div className="grid grid-cols-6  gap-8 w-full items-center">
+      <div className="grid grid-cols-6 gap-8 w-full items-center">
         <div className="col-span-2 flex items-center">
           <Image
-            className="m-auto h-96 object-cover rounded-3xl  select-none"
+            className="m-auto h-96 object-cover rounded-3xl select-none"
             src="/images/contact.png"
             alt="Picture of the author"
             width={400}
@@ -15,18 +38,17 @@ const LandingContact = () => {
         </div>
         <div className="col-span-4 flex flex-col p-10 space-y-8 mx-auto z-10">
           <div className="w-3/4">
-            <h2 className="text-3xl  font-archivo text-slate-900 dark:text-zinc-100">{`Contact Us`}</h2>
-            <h2 className=" font-archivo text-zinc-700 dark:text-zinc-100">{`Have questions or feedback? We'd love to hear from you. Please fill out the form below and we'll get back to you as soon as possible.`}</h2>
-            <form action="https://formsubmit.co/kalyanpiyushp@gmail.com" method="POST"> {/*use your emailhere */}
-
+            <h2 className="text-3xl font-archivo text-slate-900 dark:text-zinc-100">Contact Us</h2>
+            <h2 className="font-archivo text-zinc-700 dark:text-zinc-100">Have questions or feedback? We'd love to hear from you. Please fill out the form below and we'll get back to you as soon as possible.</h2>
+            <form onSubmit={sendEmail} ref={form}>
               <input
-                className="w-full bg-champange h-12 mt-4 p-4 outline-none focus:outline-orange-200 text-orange-800 font-archivo text-lg rounded-lg placeholder:text-orange-800/50 "
+                className="w-full bg-champange h-12 mt-4 p-4 outline-none focus:outline-orange-200 text-orange-800 font-archivo text-lg rounded-lg placeholder:text-orange-800/50"
                 type="text"
                 placeholder="Name"
                 required
               />
               <input
-                className="w-full bg-champange h-12 mt-4 p-4 outline-none focus:outline-orange-200 text-orange-800 font-archivo text-lg rounded-lg placeholder:text-orange-800/50 "
+                className="w-full bg-champange h-12 mt-4 p-4 outline-none focus:outline-orange-200 text-orange-800 font-archivo text-lg rounded-lg placeholder:text-orange-800/50"
                 type="email"
                 placeholder="E-mail"
                 required
@@ -42,6 +64,9 @@ const LandingContact = () => {
               >
                 Send Message
               </button>
+              {isMessageSent && (
+                <p className="text-green-500">Message sent successfully!</p>
+              )}
             </form>
           </div>
         </div>
--- firebase/index.tsx
+++ firebase/index.tsx
@@ -1,18 +1,20 @@
-import { initializeApp, getApps } from "firebase/app";
-import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAyeYkaPZvmMcvzaTEKKeGMup0on3msj3Y",
+  authDomain: "gyan-934ec.firebaseapp.com",
+  projectId: "gyan-934ec",
+  storageBucket: "gyan-934ec.appspot.com",
+  messagingSenderId: "478457042257",
+  appId: "1:478457042257:web:4e9bf45535974f85ba6f70",
+  measurementId: "G-V0LQESVYPZ"
 };
 
 let app;
