3603b0ef33e3e6b92e975c978971cd015fa061d4
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-06-03 12:50:57 +0530

    feat: Create route pages for every settings option  - settings subroute was created ( folder)  - Each page for each of the settings sidebar options was created.  - This enables easier development for frontend and backend logic for each settings page.

---
+++ pages/settings/accessibilityOptions.tsx
@@ -0,0 +1,21 @@
+import SettingsBarItem from "@/Components/SettingsBar/SettingsBarItem";
+import { HomeSidebar } from "@/Components/components";
+import SettingsBar from "@/Components/SettingsBar/SettingsBar";
+
+
+const AccessibilityOptions = () => {
+  return (
+    <div className="grid grid-cols-12 w-full h-screen bg-gradient-to-b from-slate-100 to-white">
+      <div className="col-span-2">
+        <HomeSidebar pageNumber={4} />
+      </div>
+      <div className="col-span-3 p-2">
+        <SettingsBar
+          activeItem={5}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AccessibilityOptions;
