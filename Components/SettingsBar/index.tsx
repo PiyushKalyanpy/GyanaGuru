5d4545e052a0dd832f07e5407a00997606625223
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-06-04 16:16:34 +0530

    fix: SettingsBar: Rename Component Files  - Renamed SettingsBar.tsx to index.tsx  - Renamed SettingsBarItem.tsx to Item.tsx  - Renamed SettingsSearch.tsx to Search.tsx

--- Components/SettingsBar/index.tsx
+++ Components/SettingsBar/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import SettingsBarItem from "./SettingsBarItem";
-import SettingsSearch from "./SettingsSearch";
+import SettingsBarItem from "./Item";
+import SettingsSearch from "./Search";
 
 const barItems = [
   {
