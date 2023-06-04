1a38f52ad061dd84113506a2623685313e720074
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-06-04 14:30:06 +0530

    removed css from global file and used tailwind css

--- Components/LandingFooter.tsx
+++ Components/LandingFooter.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import {AiFillGithub ,AiFillTwitterCircle ,AiFillLinkedin ,AiFillInstagram,AiFillFacebook} from "react-icons/ai"
 
 const LandingFooter = () => {
-    const hoverEffect = "flex items-center transition ease-out duration-200 hover:text-black";
+    const hoverEffect = "group flex items-center transition ease-out duration-200 hover:text-black";
     
     const currentYear = new Date().getFullYear();
     
@@ -40,20 +40,20 @@ const LandingFooter = () => {
         <div className="mb-24 mt-8">
            <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Community</div>
            <ul>
-                <li className="social github leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
                 <a href="https://github.com/PiyushKalyanpy/GyanaGuru" target="_blank" className={hoverEffect}><AiFillGithub className="mr-1" />Github</a>
                 </li>
-                <li className="social twitter leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="https:twitter.com" className={hoverEffect}><AiFillTwitterCircle className="mr-1" />Twitter</a>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                   <a href="https:twitter.com" className={hoverEffect}><AiFillTwitterCircle className="mr-1 transition ease duration-200 group-hover:text-sky-500" />Twitter</a>
                 </li>
-                <li className="social linkedin leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="https://www.linkedin.com/in/piyush-kalyan/" className={hoverEffect}><AiFillLinkedin className="mr-1" />Linkedin</a>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                   <a href="https://www.linkedin.com/in/piyush-kalyan/" className={hoverEffect}><AiFillLinkedin className="mr-1 transition ease duration-200 group-hover:text-sky-600" />Linkedin</a>
                 </li>
-                <li className="social instagram leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="https:instagram.com" className={hoverEffect}><AiFillInstagram className="mr-1" />Instagram</a>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                   <a href="https:instagram.com" className={hoverEffect}><AiFillInstagram className="mr-1 transition ease duration-200 group-hover:text-rose-500" />Instagram</a>
                 </li>
-                <li className="social facebook leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="https:facebook.com" className={hoverEffect}><AiFillFacebook className="mr-1" />Facebook</a>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                   <a href="https:facebook.com" className={hoverEffect}><AiFillFacebook className="mr-1 transition ease duration-200 group-hover:text-blue-700" />Facebook</a>
                 </li>
              </ul>
         </div>
