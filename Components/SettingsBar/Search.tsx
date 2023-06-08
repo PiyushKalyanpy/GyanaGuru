193ff5e19b28bca1dced28a7ef9302a3dbc6b83a
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-06-08 17:01:15 +0530

    fix: bug fix in search bar icon

--- Components/SettingsBar/Search.tsx
+++ Components/SettingsBar/Search.tsx
@@ -4,7 +4,7 @@ const SettingsSearch = () => {
       <label htmlFor="simple-search" className="sr-only">Search</label>
       <div className="relative w-full">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <span className="material-icons stroke-zinc-900">search</span>
+              <span className="!font-light material-symbols-rounded text-black">search</span>
           </div>
           <input type="text" id="simple-search" className="outline outline-0 focus:outline-1 outline-zinc-500 placeholder:text-zinc-500 text-zinc-900 bg-white border border-zinc-200 text-sm rounded-md block w-full pl-12 px-3 py-2" placeholder="Search" required />
       </div>
