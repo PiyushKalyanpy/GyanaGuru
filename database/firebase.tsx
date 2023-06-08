48ca8625b85f943c85cf3cdd24035ea74a066506
rajendra mandliya <119125519+mandliyarajendra11@users.noreply.github.com>
2023-06-08 19:53:31 +0530

    dark mode issue#272 solved

--- database/firebase.tsx
+++ database/firebase.tsx
@@ -6,23 +6,15 @@ import {
   signOut,
 } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyD4glkILJpJCYqgLSJDnZazOIxN2yfBpZw",
-  authDomain: "gyanaguru-raj.firebaseapp.com",
-  projectId: "gyanaguru-raj",
-  storageBucket: "gyanaguru-raj.appspot.com",
-  messagingSenderId: "83560333884",
-  appId: "1:83560333884:web:fa5feffe9dbba82a6d3939",
-  measurementId: "G-C8ND0MZYVK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 let app;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
