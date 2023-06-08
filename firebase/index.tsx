b6c4afa5ad6102090dbb8dcd5b1aee5a82d572db
Piyush Kalyan <79275157+PiyushKalyanpy@users.noreply.github.com>
2023-06-08 09:23:27 +0530

    Code cleaning

--- firebase/index.tsx
+++ firebase/index.tsx
@@ -7,14 +7,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRdGJJUKyqjsJgQs4fkDUpJdGKBha_9eg",
-  authDomain: "gyanaguru-160d9.firebaseapp.com",
-  databaseURL: "https://gyanaguru-160d9-default-rtdb.firebaseio.com",
-  projectId: "gyanaguru-160d9",
-  storageBucket: "gyanaguru-160d9.appspot.com",
-  messagingSenderId: "942292389438",
-  appId: "1:942292389438:web:c45e0e894f2406e18b8b6f",
-  measurementId: "G-0CSPV2Y0Y3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 let app;
 if (!getApps().length) {
