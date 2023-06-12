bef8c14bf4509af6c5c0c5ee889a25ac1736af3c
Piyush Kalyan <79275157+PiyushKalyanpy@users.noreply.github.com>
2023-06-12 10:17:35 +0530

    Update WhyChooseUs.tsx

--- Components/WhyChooseUs.tsx
+++ Components/WhyChooseUs.tsx
@@ -33,7 +33,7 @@ const WhyChooseUs = () => {
 
 const InfoCard = ({ title, description }: any) => {
   return (
-    <div className=" p-4 hover:bg-indigo-950 transition-colors font-archivo rounded-3xl ">
+    <div className=" p-4 hover:bg-zinc-800 transition-colors font-archivo rounded-3xl ">
       <div className="space-y-4 ">
         <h3 className="text-2xl font-semibold font-archivo">{title}</h3>
         <p className="text-black dark:text-zinc-50 font-inter ">
