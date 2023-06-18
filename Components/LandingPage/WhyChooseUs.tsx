9e32977787cc69347194ba939ff3e51b52827491
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2023-06-18 13:15:08 +0530

    added dark mode and light mode

--- Components/LandingPage/WhyChooseUs.tsx
+++ Components/LandingPage/WhyChooseUs.tsx
@@ -1,14 +1,16 @@
+
 import WhyChooseUsData from "../../data/why_choose_us.json";
 import Image from "next/image";
 import VanillaTilt from 'vanilla-tilt';
 import { useEffect, useRef } from 'react';
+import {useTheme} from "next-themes";
 
 const WhyChooseUs = () => {
   return (
     <div id="whychooseus" className="px-20 mt-40 dark:bg-neutral-950">
-      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 dark:text-zinc-50 p-4 mb-10">{`Why Choose Us`}</h1>
+      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 text-black dark:text-zinc-50 p-4 mb-10">{`Why Choose Us`}</h1>
       <div className="grid grid-cols-6  gap-8 w-full items-center">
-        <div className="col-span-4 grid grid-cols-3 gap-8 ">
+        <div className="col-span-4 grid grid-cols-3 gap-8 backgroundColor: #0A0A0A dark : backgroundColor: #000000  ">
         {WhyChooseUsData.whyChooseUs.map((item, index) => {
   const cardRef = useRef(null);
 
@@ -35,7 +37,8 @@ const WhyChooseUs = () => {
         boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Add a box shadow
         padding: '16px', // Add padding to create some space around the content
         margin: '8px', // Add margin for spacing between cards
-        backgroundColor: '#0A0A0A', // Set background color
+        
+        // backgroundColor:  '#FFFFFF', // Set background color// Set background color
       }}
     >
       <InfoCard
@@ -63,11 +66,13 @@ const WhyChooseUs = () => {
 };
 
 const InfoCard = ({ title, description }: any) => {
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === 'dark';
   return (
-    <div className=" p-4 hover:bg-zinc-800 transition-colors font-archivo rounded-3xl ">
-      <div className="space-y-4 ">
-        <h3 className="text-2xl font-semibold font-archivo">{title}</h3>
-        <p className="text-black dark:text-zinc-50 font-inter ">
+    <div className="  p-4  hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-colors font-archivo rounded-3xl ">
+      <div className="space-y-4  ">
+        <h3 className="text-2xl font-semibold font-archivo ">{title}</h3>
+        <p className={`font-inter ${isDarkMode ? 'text-white' : 'text-black'}`}>
           {description}
         </p>
       </div>
