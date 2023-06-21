45f7465c2def41272ab0609835f9f4eed0efe3fb
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2023-06-21 10:22:36 +0530

    vanila tilt with only  hnaged files

--- Components/LandingPage/WhyChooseUs.tsx
+++ Components/LandingPage/WhyChooseUs.tsx
@@ -1,25 +1,22 @@
 import WhyChooseUsData from "../../data/why_choose_us.json";
 import Image from "next/image";
+import VanillaTilt from 'vanilla-tilt';
+import { useEffect, useRef } from 'react';
+import { useTheme } from "next-themes";
 
 const WhyChooseUs = () => {
   return (
     <div id="whychooseus" className="px-20 mt-40 dark:bg-neutral-950">
-      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 dark:text-zinc-50 p-4 mb-10">{`Why Choose Us`}</h1>
+      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 text-black dark:text-zinc-50 p-4 mb-10">{`Why Choose Us`}</h1>
       <div className="grid grid-cols-6  gap-8 w-full items-center">
-        <div className="col-span-4 grid grid-cols-3 gap-8 ">
-          {WhyChooseUsData.whyChooseUs.map((item, index) => {
-            return (
-              <InfoCard
-                key={index}
-                title={item.title}
-                description={item.description}
-              />
-            );
-          })}
+        <div className="col-span-4 grid grid-cols-3 gap-8 backgroundColor:grey dark:backgroundColor: #F7EDE3">
+          {WhyChooseUsData.whyChooseUs.map((item, index) => (
+            <InfoCard key={index} title={item.title} description={item.description} />
+          ))}
         </div>
         <div className="col-span-2 flex justify-self-end">
           <Image
-            className=" "
+            className=""
             src="/images/whychooseus.png"
             alt="Picture of the author"
             width={300}
@@ -31,14 +28,44 @@ const WhyChooseUs = () => {
   );
 };
 
-const InfoCard = ({ title, description}: any) => {
+const InfoCard = ({ title, description }: any) => {
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === 'dark';
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    if (cardRef.current) {
+      VanillaTilt.init(cardRef.current, {
+        max: 15,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.5,
+      });
+    }
+  }, []);
+
   return (
-     <div className=" p-4 cursor-default hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-colors font-archivo rounded-3xl ">
-      <div className="space-y-4">
-        <h3 className="text-2xl font-semibold font-archivo">{title}</h3>
-        <p className={`font-inter text-black dark:text-white `}>{description}</p>      
+    <div
+      ref={cardRef}
+      style={{
+        display: 'inline-block',
+        width: '100%',
+        borderRadius: '4px',
+        border: '1px solid #ccc',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        padding: '16px',
+        margin: '8px',
+      }}
+    >
+      <div className="p-4 hover:bg-grey dark:hover:bg-champagne transition-colors font-archivo rounded-3xl">
+        <div className="space-y-4">
+          <h3 className="text-2xl font-semibold font-archivo">{title}</h3>
+          <p className={`font-inter ${isDarkMode ? 'text-white' : 'text-black'}`}>
+            {description}
+          </p>
         </div>
-   </div>
+      </div>
+    </div>
   );
 };
 
