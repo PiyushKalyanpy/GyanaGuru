f6b4f9d7ca4040e4c5a9708ece2acfdfd664d956
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-06-22 09:44:51 +0530

    feat: Add A11Y to SettingsBar Components  - to Search component  - to Item component

--- Components/SettingsBar/Item.tsx
+++ Components/SettingsBar/Item.tsx
@@ -5,9 +5,19 @@ const SettingsBarItem = ( { itemName, iconName, isActive, onClick } : { itemName
     <div 
       className={"flex items-center hover:bg-zinc-100 py-2 px-2 cursor-pointer rounded-md text-sm"+(isActive?" text-zinc-900 bg-zinc-100":" text-zinc-700 bg-white")}
       onClick={onClick}
+      // A11Y
+      role="button"
+      aria-pressed={isActive}
+      aria-label={`Go to ${itemName} page`}
     >
-      <span className="!font-light material-icons-outlined">{iconName}</span>
-      <p className="pl-2">{itemName}</p>
+      <span 
+        className="!font-light material-icons-outlined"
+        // A11Y
+        role="presentation" 
+      >
+        {iconName}
+      </span>
+      <p role="presentation" className="pl-2">{itemName}</p>
     </div>
   );
 }
