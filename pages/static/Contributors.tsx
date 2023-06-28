18c7501ad038e39e580fd1d20466d7eba8c9ee65
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2023-06-28 21:56:38 +0530

    Contributors.tsx

--- pages/static/Contributors.tsx
+++ pages/static/Contributors.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ContCard from './ContCard';
+
 import { useRouter } from "next/router";
 import { PageHeader, HeadingParagraph } from "@/components/components";
 const Contributors = () => {
@@ -34,28 +34,25 @@ const Contributors = () => {
         fetchContributors();
     }, []);
 
-    return (
+       return (
         <>
-             <div className="h-screen w-full">
-            <PageHeader headingText=" Meet Our Contributors " onBackClick={onBackClick}/>
+             <div className="  text-center">
+            <PageHeader  headingText=" Meet Our Contributors " onBackClick={onBackClick} />
             <HeadingParagraph/>
+            </div>
             <div className='con_container ml-6 mr-6 flex flex-wrap justify-center items-center'>
-                {contributors.map((contributor: { id: React.Key | null | undefined; avatar_url: any; login: any; contributions: any; html_url: any; }) => (
-                   
-                    <ContCard
-                        key={contributor.id}
-                        image={contributor.avatar_url}
-                        title={contributor.login}
-                        commits={contributor.contributions}
-                        profile={contributor.html_url}
-                        
-                    />
-                ))}
-                
-            </div>
-            </div>
+          {contributors.map((contributor) => (
+          
+          
+              <img src={contributor.avatar_url} alt={contributor.login} className='profile rounded-full ml-2 mt-4  hover:scale-110 drop-shadow-xl ' width="100" height="100" />
+             
+          
+          ))}
+        </div>
+        
         </>
     );
 };
+
 
 export default Contributors;
