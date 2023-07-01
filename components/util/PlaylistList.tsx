7cd01641d954f3004ae21e508e7c044c13ffa291
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-07-01 19:07:58 +0530

    bug fixed

--- components/util/PlaylistList.tsx
+++ components/util/PlaylistList.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import PlaylistCard from '../cards/PlaylistCard'
 
 const PlaylistList = ({ playlists, playlistTitle }: any) => {
-    return (
-      <div className='flex flex-col p-4 space-y-4'>
-        <h2 className='text-zinc-600'>{playlistTitle} </h2>
-        <div className='grid grid-cols-3 gap-4'>
-          {playlists &&
-            playlists.length > 0 &&
-            playlists.map((playlist: any) => (
-              <PlaylistCard key={playlist.id} playlist={playlist} />
-            ))}
-        </div>
+  return (
+    <div className='flex flex-col p-4 space-y-4'>
+      <h2 className='text-zinc-600'>{playlistTitle} </h2>
+      <div className='grid grid-cols-3 gap-4'>
+        {playlists &&
+          playlists.length > 0 &&
+          playlists.map((playlist: any) => (
+            <PlaylistCard key={playlist.id} playlist={playlist} />
+          ))}
       </div>
-    )
-  }
-  export default class PlaylistList;
+    </div>
+  )
+}
+
+export default PlaylistList
