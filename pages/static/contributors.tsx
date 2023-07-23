8d51cbeff39c5a24bfdee4e0d772699647c6be51
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-07-24 00:37:04 +0530

    updated contributors page

--- pages/static/contributors.tsx
+++ pages/static/contributors.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { BackNavButton } from '@/components/components';
-const PAGE_SIZE = 20;
+const PAGE_SIZE = 17;
+
 const Contributors = ({ contributorsData }: any) => {
   const router = useRouter();
   const [pageNumber, setPageNumber] = useState(1);
@@ -16,6 +17,25 @@ const Contributors = ({ contributorsData }: any) => {
     setPageNumber(prevPageNumber => prevPageNumber + 1);
     router.push(`/static/contributors?page=${Number(pageNumber) + 1}`);
   };
+
+  const adminAndMentors = [
+    {
+      avatar_url: 'https://avatars.githubusercontent.com/u/79275157?v=4',
+      login: 'Piyush Kalyan',
+      userType: 'Project Admin',
+    },
+    {
+      avatar_url: 'https://avatars.githubusercontent.com/u/10762218?v=4',
+      login: 'Lalit Kumar',
+      userType: 'Mentor',
+    },
+    {
+      avatar_url: 'https://avatars.githubusercontent.com/u/89184872?v=4',
+      login: 'Vaishnavi Mokadam',
+      userType: 'Mentor',
+    },
+  ];
+
   return (
     <div className='w-screen h-screen p-8 py-8 space-y-4 overflow-y-scroll  bg-zinc-100'>
       <div className='flex flex-col md:flex-row md:items-center space-x-4 justify-between'>
@@ -40,10 +60,33 @@ const Contributors = ({ contributorsData }: any) => {
           </button>
         </div>
       </div>
+      <div className='flex flex-col gap-4'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 w-fit self-center'>
+      {adminAndMentors.map((contributor: any) => {
+          return (
+            <UserCard
+              key={contributor.login}
+              login={contributor.login}
+              avatar_url={contributor.avatar_url}
+              userType={contributor.userType}
+            />
+          );
+        })}
+      </div>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-        {contributorsData.map((contributor: any) => (
-          <UserCard key={contributor.login} {...contributor} />
-        ))}
+        
+        {contributorsData.map((contributor: any) => {
+          return (
+            <UserCard
+              key={contributor.id}
+              login={contributor.login}
+              avatar_url={contributor.avatar_url}
+              contributionsURL={contributor.contributions_url}
+              numReposContributed={contributor.contributions}
+            />
+          );
+        })}
+      </div>
       </div>
     </div>
   );
@@ -51,21 +94,54 @@ const Contributors = ({ contributorsData }: any) => {
 const UserCard = ({
   login,
   avatar_url,
-  contributions,
+  userType,
   contributionsURL,
   numReposContributed,
 }: any) => {
+  const color =
+    userType && userType === 'Project Admin'
+      ? 'bg-cyan-100 text-cyan-800'
+      : 'bg-violet-100 text-violet-800';
+
   return (
-    <div className='items-center p-4 transition bg-white rounded-2xl hover:scale-105'>
-      <img
-        src={avatar_url}
-        alt={`Profile of ${login}`}
-        className='w-16 h-16 mx-auto mb-4 rounded-full'
-      />
-      <h2 className='text-xl font-semibold text-center font-urbanist'>
-        {login}
-      </h2>
-    </div>
+    login != 'PiyushKalyanpy' && (
+      <div className='relative p-4 transition bg-white rounded-2xl hover:scale-105'>
+        {/* {userType && (
+          <div className=' cursor-pointer absolute top-4 right-4  p-2 text-xs font-bold text-white bg-blue-100  rounded-full hover:scale-110 transition '>
+            <div className='flex items-center'>
+              <span className='material-symbols-outlined text-blue-600 m-auto -rotate-45'>
+                link
+              </span>
+            </div>
+          </div>
+        )} */}
+        <div className='flex space-y-2  flex-col  items-center'>
+          <img
+            src={avatar_url}
+            alt={`Profile of ${login}`}
+            className='w-16 h-16 mx-auto mb-4 rounded-full'
+          />
+          {
+            // tag
+            (userType && userType === 'Project Admin') ||
+            userType === 'Mentor' ? (
+              <div
+                className={`px-2 text-violet-600 bg-violet-100  rounded-full ${color}`}
+              >
+                {userType}
+              </div>
+            ) : (
+              <div className='px-2 text-zinc-900 bg-zinc-100  rounded-full'>
+                Contributor
+              </div>
+            )
+          }
+          <h2 className='text-xl font-semibold text-center font-urbanist'>
+            {login}
+          </h2>
+        </div>
+      </div>
+    )
   );
 };
 export default Contributors;
