1b7b02b6db81ff586703423ed733ad69e1cc918f
Tushar Saini <s2sharpit@gmail.com>
2023-07-25 08:52:21 +0530

    improving page.
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/pages/static/faq.tsx
+++ src/pages/static/faq.tsx
@@ -8,6 +8,7 @@ import {
   AccordionIcon,
 } from '@chakra-ui/accordion';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function FAQ () {
   return (
@@ -15,8 +16,8 @@ export default function FAQ () {
       <div className="fixed z-40 w-full transition">
         <LandingNav />
       </div>
-      <main className="px-6 py-12 md:px-12 max-w-7xl mx-auto grid gap-20">
-        <section className="mt-20 text-center grid gap-3 place-items-center text-zinc-900">
+      <main className="px-6 py-12 md:px-12 max-w-7xl mx-auto grid gap-24">
+        <section className="mt-24 text-center grid gap-5 place-items-center text-zinc-900">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold max-w-4xl">
             Gyanaguru’s Frequently Asked Question
           </h1>
@@ -35,13 +36,13 @@ export default function FAQ () {
 
 function FaqSection () {
   return (
-    <div className="space-y-20">
+    <div className="space-y-24">
       <form className="grid gap-4 max-w-xs mx-auto border-b border-zinc-300">
-        <div className="flex items-center gap-2 rounded p-1">
-          <span className="material-icons">search</span>
+        <div className="flex items-center gap-2 rounded p-1 cursor-pointer">
+          <span className="material-symbols-outlined">search</span>
           <input
             type="text"
-            className="h-8 outline-none w-full"
+            className="h-8 outline-none w-full cursor-pointer"
             placeholder="Search"
           />
         </div>
@@ -54,7 +55,7 @@ function FaqSection () {
                 <AccordionButton className="text-lg font-semibold flex justify-between gap-2">
                   <h3 className="text-left">{faq.question}</h3>
                   <span
-                    className={`material-icons text-zinc-500 transition ${
+                    className={`material-symbols-outlined text-zinc-500 transition ${
                       isExpanded && 'rotate-90'
                     }`}
                   >
@@ -78,7 +79,7 @@ function FaqSection () {
 function StillHaveQuestions() {
   return (
     <section className="bg-zinc-900 text-zinc-200 px-8 lg:px-16 pt-10 rounded-3xl flex flex-wrap lg:flex-nowrap justify-between">
-      <div className="pb-8 w-full lg:w-[50%]">
+      <div className="pb-8 w-full lg:w-1/2">
         <h3 className="text-zinc-100 text-3xl font-semibold">
           Still have questions?
         </h3>
@@ -86,11 +87,19 @@ function StillHaveQuestions() {
           We're here to help! Reach out to us for any further inquiries or
           assistance.
         </p>
-        <div className="flex gap-4 mt-6">
-          <button className="bg-zinc-800 px-4 py-2 rounded-xl">
+        <div className="flex gap-4 mt-6 text-center">
+          <Link
+            href="/about-us"
+            className="bg-zinc-800 px-4 py-2 rounded-xl hover:bg-zinc-950 duration-300"
+          >
             Learn More
-          </button>
-          <button className="border px-4 py-2 rounded-xl">Contact Us</button>
+          </Link>
+          <Link
+            href="/#contact"
+            className="border px-4 py-2 rounded-xl hover:bg-zinc-950 duration-300"
+          >
+            Contact Us
+          </Link>
         </div>
       </div>
       <div className="w-full lg:w-[50%] self-end mt-4 lg:mt-0 flex justify-end">
@@ -99,7 +108,7 @@ function StillHaveQuestions() {
           src="/images/faqsection.png"
           width={800}
           height={600}
-          alt=""
+          alt="screenshot of gyanaguru website"
         />
       </div>
     </section>
