7ec559b8a3ed69e4570458eff2a761e41a401ef1
Tushar Saini <s2sharpit@gmail.com>
2023-07-26 21:01:14 +0530

    making WhyChooseUs section responsive
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/components/landing/WhyChooseUs.tsx
+++ src/components/landing/WhyChooseUs.tsx
@@ -6,10 +6,10 @@ import WhyChooseUsData from "../../data/why_choose_us.json";
 
 const WhyChooseUs = () => {
   return (
-    <div id="whychooseus" className="px-20 mt-40 dark:bg-neutral-950">
-      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 text-black dark:text-zinc-50 p-4 mb-10">{`Why Choose Us`}</h1>
-      <div className="grid grid-cols-6 gap-8 w-full items-center">
-        <div className="col-span-4 grid grid-cols-3 gap-8 backgroundColor:grey dark : backgroundColor: #F7EDE3">
+    <div id="whychooseus" className="px-8 sm:px-20 mt-40 dark:bg-neutral-950">
+      <h1 className="text-4xl text-center font-semibold font-archivo text-slate-900 dark:text-zinc-50 p-4 mb-10">Why Choose Us</h1>
+      <div className="grid lg:grid-cols-6 gap-8 w-full items-center">
+        <div className="col-span-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 backgroundColor:grey dark : backgroundColor: #F7EDE3">
           {WhyChooseUsData.whyChooseUs.map((item, index) => {
             const cardRef = useRef(null);
 
@@ -27,15 +27,8 @@ const WhyChooseUs = () => {
             return (
               <div
                 ref={cardRef}
-                style={{
-                  display: 'inline-block',
-                  width: '100%',
-                  borderRadius: '4px',
-                  border: '1px solid #ccc',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                  padding: '16px',
-                  margin: '8px',
-                }}
+                key={index}
+                className="inline-block w-full rounded border border-stone-300 shadow p-4"
               >
                 <InfoCard
                   key={index}
@@ -46,9 +39,8 @@ const WhyChooseUs = () => {
             );
           })}
         </div>
-        <div className="col-span-2 flex justify-self-end">
+        <div className="col-span-4 sm:col-span-3 md:col-span-2 flex lg:justify-self-end">
           <Image
-            className=" "
             src="/images/whychooseus.png"
             alt="Picture of the author"
             width={300}
