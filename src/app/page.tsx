fc1c2143d0d29b96c29e1611871f5e1f0a695d2a
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2024-04-14 20:36:53 +0530

    removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import Background from "@/components/Background";
 import HomeNav from "@/components/navbar/HomeNav";
 import HeroSection from "@/components/landing/HeroSection";
 
