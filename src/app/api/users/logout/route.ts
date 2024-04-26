15cc038b7a8a70888f12c9846417bc4cb78e90d2
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2024-04-26 15:21:15 +0530

    api errors

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,21 @@
+import { connectToMongo } from "@/database/mongo";
+import { NextRequest, NextResponse } from "next/server";
+
+connectToMongo();
+
+export async function GET(request: NextRequest, response: NextResponse) {
+  try {
+    const response = NextResponse.json(
+      { message: "Logout successful" },
+      { status: 200 },
+    );
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    return response;
+    return response
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
